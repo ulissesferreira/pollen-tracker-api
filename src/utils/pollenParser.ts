96a4d970f2a6b9d2cf50ab1f3da6c3a0da44d89c
Ulisses Ferreira <ulissesvf@gmail.com>
2020-03-01 16:15:41 +0000

    feat: create Pollen Parser class

---
+++ src/utils/pollenParser.ts
@@ -0,0 +1,70 @@
+import cheerio from 'cheerio'
+
+const POLLENS_LIST = [
+  'Artemisia',
+  'Azeda',
+  'Azinheira',
+  'Bétula',
+  'Carvalhos',
+  'Castanheiro',
+  'Cipreste',
+  'Erva-de-orelha',
+  'Eucalipto',
+  'Funcho',
+  'Gramíneas',
+  'Oliveira',
+  'Palmeira',
+  'Parietária',
+  'Pinheiro',
+  'Plátano',
+  'Quenopódio',
+  'Salgueiro',
+  'Tanchagem',
+  'Urtiga'
+]
+
+export interface PollenData {
+  level: string,
+  pollens: string[]
+}
+
+export class PollenParser {
+
+  /*
+  ** Parses incoming HTML from https://www.rpaerobiologia.com/previsao-polinica/lisboa
+  ** And returns structured data
+  */
+  parseHTML(inputHTML: string): PollenData {
+    const $: CheerioStatic = cheerio.load(inputHTML, { decodeEntities: false })
+    const level: string = $('.previsao-text')
+      .text()
+      .trim()
+      .split('os pólenes encontram-se em níveis ')[1]
+      .split(',')[0]
+    const pollensString: string = $('.previsao-text')
+      .text()
+      .trim()
+      .split('com predomínio dos pólenes')[1]
+      .split('.')[0]
+  
+    const pollens = this.getDataPollens(POLLENS_LIST, pollensString)
+  
+    return {
+      level,
+      pollens
+    }
+  }
+
+  /*
+  ** Returns true if the data string parameter has the pollen
+  ** name in it.
+  */
+  getDataPollens(pollensList: string[], data: string): string[] {
+
+    let presentPollens: string[] = pollensList.filter((pollen) => {
+      return data.includes(pollen.toLowerCase())
+    })
+  
+    return presentPollens
+  }
+}
